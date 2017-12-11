b0a71da121c8abbc0d748af881e66ba1a05d3f6a
Leo <33761813+Leontii888@users.noreply.github.com>
2017-12-11 15:50:10 +0300

    add change

--- js/main.js
+++ js/main.js
@@ -37,8 +37,7 @@ $(document).ready(function(){
 			function(){
 				$(this).removeClass('animated jackInTheBox').css("display","none");
 			});
-		
-	});
+		});
 
 	// $('.j-price').on('click', function(){
 	// 	$('.j-advantages__item_hidden').addClass('animated jackInTheBox j-intro--color');
@@ -74,7 +73,27 @@ $(document).ready(function(){
 		
 		// function(){
 		// 	$(this).animate({left: "200%"}, "fast")};
-
+   function checkForm(form) {
+                var name = form.name.value;
+                var n = name.match(/^[A-Za-zА-Яа-я ]*[A-Za-zА-Яа-я ]+$/);
+                if (!n) {
+                    alert("Имя введено неверно. Пожалуйста, исправьте ошибку");
+                    return false;
+                }
+                var phone = form.phone.value;
+                var p = phone.match(/^[0-9+][0-9- ]*[0-9- ]+$/);
+                if (!p) {
+                    alert("Телефон введен неверно");
+                    return false;
+                }
+                var mail = form.mail.value;
+                var m = mail.match(/^[A-Za-z0-9][A-Za-z0-9\._-]*[A-Za-z0-9_]*@([A-Za-z0-9]+([A-Za-z0-9-]*[A-Za-z0-9]+)*\.)+[A-Za-z]+$/);
+                if (!m) {
+                    alert("E-mail введен неверно, пожалуйста исправьте ошибку");
+                    return false;
+                }
+                return true;
+            }
 
 
 });
